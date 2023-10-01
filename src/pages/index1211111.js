ab03c9558dcbd671e3c7d1c8b678feb2667b7938
xMetals-Dapp <126717088+xMetals-Dapp@users.noreply.github.com>
2023-10-01 17:38:49 -0400

    Update index1211111.js

--- src/pages/index1211111.js
+++ src/pages/index1211111.js
@@ -490,7 +490,7 @@ try{
 						focus-box-shadow="0 0 0 0px --color-primary"
 						onClick ={mintTransaction}
 					>
-						Redeem Physical
+						Redeem
 					</Button>
 				</Box>
 				<Box width="75%">
