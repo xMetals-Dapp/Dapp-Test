37f8e899923e3f531d124f6ec80f8a9dfbe0f522
xMetals-Dapp <126717088+xMetals-Dapp@users.noreply.github.com>
2023-10-02 13:05:22 -0400

    Update index1211111.js

--- src/pages/index1211111.js
+++ src/pages/index1211111.js
@@ -528,7 +528,7 @@ try{
 				border-radius="30px"
 				align-items="center"
 			>
-				<Image src="https://uploads.quarkly.io/63e1507af2f031001fb80741/images/xGLD.jpg?v=2023-02-07T19:13:55.114Z" display="block" width="100%" border-radius="30px 30px 0px 00px" />
+				<Image src="https://uploads.quarkly.io/63e1507af2f031001fb80741/images/GoldKilo.jpg?v=2023-04-21T18:44:41.005Z" display="block" width="100%" border-radius="30px 30px 0px 00px" />
 				<Box />
 				<Box display="flex" flex-direction="column" align-items="center" justify-content="center">
 					<Box height="5%" />
