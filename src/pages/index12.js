6d606d4e1393ef60d831bfd1dcc353039d09146f
xMetals-Dapp <126717088+xMetals-Dapp@users.noreply.github.com>
2023-10-02 13:01:21 -0400

    Update index12.js

--- src/pages/index12.js
+++ src/pages/index12.js
@@ -572,10 +572,10 @@ try{
 						width="95%"
 					>
 						<br />
-						Physical bar will be shipped to address on KYC
+						Physical bar will be shipped to address on KYC.
 						<br />
 						<br />
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+						You will receive a 1 troy ounce (31.1035g) Platinum bar from a LBMA/LPPM approved manufacturer, guaranteed to be .9995 pure. The bar(s) come in sealed/secure packaging and shipping is insured, with a tracking number being provided. Brands are likely to be Valcambi, Argor-Heraeus, Baird or PAMP.
 					</Text>
 					<Box height="40%" />
 				</Box>
